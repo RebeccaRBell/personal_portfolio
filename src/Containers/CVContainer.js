589e70febd2d2103bd09e98a4a1d53fa3d28d786
Rebecca Bell <rebeccaromanobell@gmail.com>
2023-04-05 19:28:26 +0100

    CV updates including langauges and frameworks section

--- src/Containers/CVContainer.js
+++ src/Containers/CVContainer.js
@@ -6,6 +6,10 @@ import shecodesbasic from '../media/shecodesbasic.png';
 import shecodesplus from '../media/shecodesplus.png'
 import shecodesresponsive from '../media/shecodesresponsive.png';
 import shecodesreact from '../media/shecodesreact.png';
+import pythonlogo from '../media/pythonlogo.png';
+import jslogo from '../media/jslogo.png';
+import reactlogo from '../media/reactlog.png';
+import javalogo from '../media/javalogo.png'
 
 const CVContainer = () => {
   return (
@@ -13,28 +17,43 @@ const CVContainer = () => {
     <NavBar />
     <div className='cv-body'>
       <h1>cv</h1>
+      <div className='row-one-container'>
       <div className='coding-experience'>
-      <div className='coding-text-cont'>
       <h3>coding experience</h3>
-      <div className='coding-text'>
+      <div className='coding-experience-section'>
       <h4>CodeClan - Professional Software Development</h4>
       <p>SQA - Professional Development Award<br />
       Jan 2023 - Apr 2023</p>
       <ul>
-        <li>Accredited Software Development Bootcamp teaching Python, JavaScript, React, Java, Spring, SQL, MongoDB, Flask, and Express.js</li>
-        <li>Designed and programmed a solo full-stack Python project, a group full-stack JS/React project, and a group full-stack Java & React project</li>
+        <li> - Accredited Software Development Bootcamp teaching Python, JavaScript, React, Java, Spring, SQL, MongoDB, Flask, and Express.js</li>
+        <li> - Designed and programmed a solo full-stack Python project, a group full-stack JS/React project, and a group full-stack Java & React project</li>
       </ul>
-      <br />
+      </div>
+      <div className='coding-experience-section'>
       <h4>SheCodes - SheCodes Pro Programme</h4>
       <p>Online Bootcamp focussed on making tech more accessible to women.<br />
       Oct 2022 - Dec 2022
       </p>
       <ul>
-        <li>Completed all SheCodes courses included in their Pro Programme which focussed on Front-End Design and Development</li>
-        <li>Designed and programmed a weather app using React</li>
+        <li>- Completed all SheCodes courses included in their Pro Programme which focussed on Front-End Design and Development</li>
+        <li>- Designed and programmed a weather app using React</li>
       </ul>
      </div>
      </div>
+     
+     <div className='languages'>
+        <h3>languages & frameworks</h3>
+        <div className='languages-section'>
+    <img src={reactlogo} alt='React Logo'/>
+    <img src={jslogo} alt='JavaScript Logo'/>
+    <img src={pythonlogo} alt='Python Logo'/>
+    <img src={javalogo} alt='Java Logo'/>
+          </div>
+        </div>
+      </div>
+
+ <div className='row-two-container'>
+ <div className='row-two'>
       <div className='certificates'>
         <h3>certifications</h3>
         <div className='cert-images'>
@@ -43,9 +62,10 @@ const CVContainer = () => {
         <img src={shecodesplus} alt='SheCodes Plus Certificate'/>
           <img src={shecodesbasic} alt='SheCodes Basic Certificate'/>
           
+
+      </div>
         </div>
       </div>
-        
       </div>
     </div>
     <Footer />
