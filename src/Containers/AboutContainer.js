e199653ef99df38c0912e70b4f0bb9c5b553dffa
Rebecca Bell <rebeccaromanobell@gmail.com>
2023-05-04 16:37:49 +0100

    content completed

--- src/Containers/AboutContainer.js
+++ src/Containers/AboutContainer.js
@@ -2,32 +2,57 @@ import React from 'react'
 import NavBar from '../Components/NavBar';
 import './AboutContainer.css';
 import bristol from '../media/bristol.jpg';
+import prompt from '../media/bristolprompt.jpeg';
+import lighting from '../media/lightingdesk.jpg';
 import Footer from '../Components/Footer';
 
 const AboutContainer = () => {
   return (
-    <div className='about-container'>
-    <NavBar />
-    <div className='about-body'>
-      <h1>About Me</h1>
-      <div className='about-text-one'>
-        <p>
-          I stumbled across the world of software and web development around a year ago and, as cheesy as it sounds, I haven't stopped coding since. With my background in Music and Theatre Production I've always had an interest in how creativity and technology can combine. 
-          After working as a freelance Stage Manager for 5 year and feeling pretty unchallenged and uninspired in the jobs I was doing, I was ready for a change of career. <br /><br />
-          I saw an ad for SheCodes' <i>Introduction to Coding</i> workshop and quickly discovered that web and software development was the perfect way to combine my creativity with my problem-solving skills and eye for detail. <br/>
-          I then went on to complete SheCodes' Pro programme which is a front-end programming and design course, where I got to to grips with HTML, CSS, JavaScript and React.
-          After completing the course I felt I needed do something else to broaden my base knowledge of software development as I felt I didn't have a solid enough grounding purely from the SheCodes programme, which is where CodeClan comes in. <br /><br />
-          CodeClan was recommended to me first-hand from someone who had gone through a career change and was able to make get that first developer role after completing CodeClan's Professional Software Development course.
-          
-        </p>
-        <div className='image-one'>
-        <img src={bristol} alt='Bristol Old Vic auditorium'/>
-        </div>
-      </div>
-    </div>
-    <Footer />
-    </div>
-  )
+		<div className="about-container">
+			<NavBar />
+			<div className="about-body">
+				<h1>About Me</h1>
+				<div className="about-text-one">
+					<div className="images">
+						<div className="image-one">
+							<img src={prompt} alt="Bristol Old Vic auditorium" />
+						</div>
+						<div className="image-two">
+							<img src={bristol} alt="Bristol Old Vic auditorium" />
+						</div>
+						<div className="image-three">
+							<img src={lighting} alt="Bristol Old Vic auditorium" />
+						</div>
+					</div>
+					<p>
+						With my background in Music and Theatre Production I've
+						always had an interest in how creativity and art can be advanced with technology.
+						After working as a freelance Stage Manager for 5 years and feeling
+						pretty unchallenged and uninspired in the jobs I was doing, I was
+						ready for a change of career. <br />
+						<br />I found SheCodes' Introduction to Coding
+						workshop and quickly discovered that web and software development
+						seemed to be the perfect way I could combine my creativity with my problem-solving
+						skills and eye for detail. <br />
+						I then went on to complete SheCodes' Pro programme which is a
+						front-end programming and design course, where I got to grips
+						with HTML, CSS, JavaScript and React. After completing the course, I
+						felt I needed further study to broaden my base knowledge of
+						software development as I felt I didn't have a solid enough
+						grounding purely from the SheCodes programme. This is where
+						CodeClan comes in. <br />
+						<br />
+						CodeClan was recommended to me first-hand from someone who had gone
+						through a career change and was able to make the transition into their first developer role after completing CodeClan's Professional Software
+						Development course. And I'm so grateful it was recommended to me when it was. Attending an in-person software development bootcamp was the best thing I could have done at that point. It was an intense 16-weeks and really pushed me to challenge myself and really throw myself into this world I had really only scratched the surface of. <br />
+            And now we're here. I've recently graduated from CodeClan, and am searching for my first developer role. 
+            If you'd like to talk to me about anything I've mentioned or about any opportunities you think I might be a good fit for, please don't hesitate to get in touch!
+					</p>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default AboutContainer;
--- src/Containers/ContactContainer.js
+++ src/Containers/ContactContainer.js
@@ -2,20 +2,49 @@ import React from 'react'
 import NavBar from '../Components/NavBar'
 import './ContactContainer.css'
 import Footer from '../Components/Footer'
+import ReactDOM from 'react-dom';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 
 const ContactContainer = () => {
+  const email = 'rebeccaromanobell@gmail.com';
   return (
-    <div className='contact-container'>
-        <NavBar />
-        <div className='contact-body'>
-        <h1>Contact Me</h1>
-        <div className='contact-backing'>
-        <p> If you'd like to get in touch, you can reach me via <a href='https://www.linkedin.com/in/rebeccarbell/' target='_blank' rel='noreferrer'>linkedin</a> or at rebeccaromanobell@gmail.com</p>
-        </div>
-        </div>
-        <Footer />
-    </div>
-  )
+		<div className="contact-container">
+			<NavBar />
+			<div className="contact-body">
+				<h1>Contact Me</h1>
+				<div className="contact-backing">
+					<p>
+						{" "}
+						If you'd like to get in touch, please don't hesitate to contact me through the options below (plus a link to my github for good measure)!
+					</p>
+					<div className="icons-contact">
+						<a
+							href="https://www.linkedin.com/in/rebeccarbell/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FontAwesomeIcon icon={faLinkedin} className="linkedin" />
+						</a>
+						<a
+							href="https://github.com/RebeccaRBell"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FontAwesomeIcon icon={faGithub} className="github" />
+						</a>
+						<a href="mailto:${email}" target="_blank" rel="noreferrer">
+							<FontAwesomeIcon icon={faSquareEnvelope} className="envelope" />
+						</a>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default ContactContainer;
