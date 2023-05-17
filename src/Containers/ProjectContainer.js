6f8b1fae26a853c1a3349985e7cecac895ac6024
Rebecca Bell <rebeccaromanobell@gmail.com>
2023-05-17 13:25:55 +0100

    added link to hosted weather app project

--- src/Containers/ProjectContainer.js
+++ src/Containers/ProjectContainer.js
@@ -7,6 +7,8 @@ import smeco from "../media/smeco.mp4";
 import summit from "../media/summit.mp4";
 import github from "../media/github.svg";
 import weather from '../media/weather.mp4';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 const ProjectContainer = () => {
 
@@ -224,7 +226,7 @@ const ProjectContainer = () => {
 								<div className="project-two-text weather-text">
 									<h4>React | OpenWeather API</h4>
 									<p>
-										This app was created originally created during my time
+										This app was originally created during my time
 										studying with SheCodes on their React course.
 										<br />
 										After graduating from CodeClan I decided to give the project
@@ -271,6 +273,13 @@ const ProjectContainer = () => {
 											>
 												<path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
 											</svg>
+										</a>
+										<a
+											href="https://beccas-weather-app.netlify.app"
+											target="_blank"
+											rel="noreferrer"
+										>
+											<FontAwesomeIcon icon={faLink} />
 										</a>
 									</div>
 								</div>
