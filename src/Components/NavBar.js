35222927e0836adf9c8c1aa1c58831714f2d6961
Rebecca Bell <rebeccaromanobell@gmail.com>
2023-05-19 20:54:04 +0100

    icons in mobile view fixed

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -17,7 +17,7 @@ const NavBar = () => {
    <NavLink to='/'> <div className='name-logo'><p className='letter-r'>R</p><p className='letter-b'>B</p></div></NavLink>
         <ul className='nav-list'>
                 <li> <NavLink  exact to='/'><FontAwesomeIcon icon="fa-solid fa-house"/></NavLink></li>
-                <li><NavLink   activeClassName="active"  className='highlight-project' to='/projects'><FontAwesomeIcon icon='fa-solid fa-pencil'/></NavLink></li>
+                <li><NavLink activeClassName='active' className='highlight-project' to='/projects'><FontAwesomeIcon icon='fa-solid fa-pencil'/></NavLink></li>
                 <li><NavLink activeClassName='active' className='highlight-about' to='/about'><FontAwesomeIcon icon='fa-solid fa-circle-user'/></NavLink></li>
                 <li><NavLink activeClassName='active' className='highlight-cv' to='/cv'><FontAwesomeIcon icon='fa-solid fa-file'/></NavLink></li>
                 <li><NavLink activeClassName='active' className='highlight-contact' to='/contact'><FontAwesomeIcon icon='fa-solid fa-square-phone-flip'/></NavLink></li>
